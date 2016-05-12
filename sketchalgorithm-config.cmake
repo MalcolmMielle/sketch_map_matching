5a71a340869434f2338f21f2c642d63618085bb5
Malcolm Mielle <malcolm.mielle@protonmail.com>
2016-05-12 09:44:35 +0200

    corrected lib names

--- sketchalgorithm-config.cmake
+++ sketchalgorithm-config.cmake
@@ -1,4 +1,4 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(sketchAlgorithm_INCLUDE_DIRS "${SELF_DIR}/../../include/sketchAlgorithm" ABSOLUTE)
-set(sketchAlgorithm_LIBRARIES "${SELF_DIR}/Comparatorlib.a;${SELF_DIR}/Probabilitieslib.a;${SELF_DIR}/libVFLib.a")
+set(sketchAlgorithm_LIBRARIES "${SELF_DIR}/libComparatorlib.a;${SELF_DIR}/libProbabilitieslib.a;${SELF_DIR}/libVFLib.a")
