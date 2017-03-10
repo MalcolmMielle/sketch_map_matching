5adb955424875d6f680c3e540c6cb2e1989803b0
Malcolm Mielle <malcolm.mielle@protonmail.com>
2017-03-10 16:05:01 +0100

    added RSI dependency + conversin file

---
+++ includes/Conversion/RSIConversion.hpp
@@ -0,0 +1,20 @@
+#ifndef SKETCHALGO_RSICONVERSION_10032017
+#define SKETCHALGO_RSICONVERSION_10032017
+
+#include "RSI/GraphZone.hpp"
+#include "GraphPlace.hpp"
+
+
+	
+namespace AASS{
+		
+	namespace graphmatch{
+		
+		void graphZoneToGraphPlace(const AASS::RSI::GraphZone& graph_zone, AASS::graphmatch::GraphPlace& grapl_place){
+			
+			
+			
+		}		
+	}
+	
+}
